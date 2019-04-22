2685e07ee77ca9ca7628d176e753411e934822de
kaviraj333 <49511634+kaviraj333@users.noreply.github.com>
2019-04-22 11:58:34 +0530

    r1.c

---
+++ r1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+ {
+  int n;
+  {
+    printf("Enter the number:");
+    scanf("%d",&n);
+    if(n%2==0)
+     {
+      printf("The Even number is:%d",n);
+      }
+      else
+      {
+      printf("The Odd number is:%d",n);
+      }
+    }
+    getch();
+    }
