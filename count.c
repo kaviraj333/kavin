e39a700da179632041bcb003415208e391f2e126
kaviraj333 <49511634+kaviraj333@users.noreply.github.com>
2019-05-07 13:20:34 +0530

    count.c

---
+++ count.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+  {
+  int i,n,a[10];
+  printf("Enter the number:");
+  scanf("%d",&n);
+  for(i=0;i<=n;i++)
+    {
+     scanf("%d",&a[i]);
+     }
+     printf("The Total number is:%d",n);
+     return 0;
+    } 
