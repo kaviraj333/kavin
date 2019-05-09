4911199ccd4ce5779d48bdaf10c9d36380934718
kaviraj333 <49511634+kaviraj333@users.noreply.github.com>
2019-05-09 11:29:08 +0530

    Multipleof5.c

---
+++ Multipleof5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+longint sum=0,i,n;
+clrscr()
+printf("\n Please Give The Value of N: ");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+printf("\n 5 * %Id",i,5*i);
+}
+getch();
+}
