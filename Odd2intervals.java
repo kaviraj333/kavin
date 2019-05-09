ecc448d8ff928ae0391699ec1de6f2b30b044a61
kaviraj333 <49511634+kaviraj333@users.noreply.github.com>
2019-05-09 11:48:44 +0530

    Odd2intervals.java

---
+++ Odd2intervals.java
@@ -0,0 +1,14 @@
+import java.io.*;
+class Oddnumber{
+public static voidmain(String args[])
+{
+System.out.println(String args[])
+{
+for(inti=1;i<=100;i++)
+{
+if(i%2!=0)
+{
+System.out.println(i+" ");
+}
+}
+}
