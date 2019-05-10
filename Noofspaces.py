3de394ed89687214fcf8f74192dbf9c1a1440f0e
kaviraj333 <49511634+kaviraj333@users.noreply.github.com>
2019-05-10 11:17:17 +0530

    Noofspaces.py

---
+++ Noofspaces.py
@@ -0,0 +1,10 @@
+class ch:
+    def __init__(self):
+    self.s=str(input())
+            l=len(self.s)
+            c=0
+              for i in range(0,l-1):
+                   if(self.s[i]==' '):
+                   c=c+1
+                   print(c)
+                   o=ch()
