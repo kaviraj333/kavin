376e9766fb820fbceffc932691fd2601b5d466da
kaviraj333 <49511634+kaviraj333@users.noreply.github.com>
2019-05-14 15:02:18 +0530

    104.java

---
+++ 104.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class B104
+{
+public static void main(String[] args) 
+{
+Scanner sj = new Scanner(System.in);
+ int d=sj.nextInt();
+ int p=sj.nextInt();
+ System.out.print(Math.round(Math.pow(d, p)));
+
+}
+}
