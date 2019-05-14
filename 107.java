a146ed1254682400baa6920279780062cd40f663
kaviraj333 <49511634+kaviraj333@users.noreply.github.com>
2019-05-14 15:11:27 +0530

    107.java

---
+++ 107.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class B107
+{
+public static void main(String[] args) 
+{
+Scanner sj = new Scanner(System.in);
+ int a=sj.nextInt();
+ int b=sj.nextInt();
+ int c=sj.nextInt();
+System.out.print((a*b)/c);
+
+
+}
+}
