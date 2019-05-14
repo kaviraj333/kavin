08e51a383b6adc16990f80c1f4f4157b4cd6d73a
kaviraj333 <49511634+kaviraj333@users.noreply.github.com>
2019-05-14 14:20:43 +0530

    100.java

---
+++ 100.java
@@ -0,0 +1,16 @@
+    
+import java.util.Scanner;
+class Digits
+{
+    public static void main(String args[]){
+        int m, n, sum = 1;
+        Scanner s = new Scanner(System.in);
+        m = s.nextInt();
+        while(m > 0){
+            n = m % 10;
+            sum = sum * n;
+            m = m / 10;
+        }
+        System.out.println(sum);
+    }
+}
