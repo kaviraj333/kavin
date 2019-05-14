adf1f18481baf497ee21a890300168457e4812d4
kaviraj333 <49511634+kaviraj333@users.noreply.github.com>
2019-05-14 14:01:16 +0530

    95.java

---
+++ 95.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Floorvalue
+{
+	
+	public static void main(String[] args) 
+	{
+		int p,r,t;
+        Scanner	sj= new Scanner(System.in);
+        p=sj.nextInt();
+        t=sj.nextInt();
+        r=sj.nextInt();
+        System.out.print((r*t*p)/100);
+       
+	}
+}
